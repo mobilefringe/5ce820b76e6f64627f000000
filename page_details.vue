aa9099f6824f408a4ac03485269c54d9baf4298f
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2019-05-24 14:36:42 -0400

    High Desert Gateway & West 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(39, 88, 12, 0.8), rgba(39, 88, 12, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgb(39, 88, 12)' }">
+                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(39, 88, 112, 0.8), rgba(39, 88, 112, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgb(39, 88, 112)' }">
                     <div class="main_container">
                         <h2 v-if="currentPage.title" v-html="currentPage.title"></h2>
                     </div>
